83feb424958b0cb712200e003e48a5326374363c
panghua <panghua@le.com>
2021-04-13 15:30:58 +0800

    旋转数组的最小值

--- Calculate_Kotlin/src/main/java/kotlin/arryaOp/offer11.kt
+++ Calculate_Kotlin/src/main/java/kotlin/arryaOp/offer11.kt
@@ -1,8 +1,8 @@
 package arryaOp
 
 fun main() {
-    val array = intArrayOf(2,2,2,0,1)
-    print(minArray(array))
+    val array = intArrayOf(2,2)
+    print(findMin(array))
 
 }
 
@@ -23,22 +23,15 @@ fun minArray(numbers: IntArray): Int {
 
 // 2.二分查找
 fun findMin(numbers: IntArray): Int {
-    if(numbers == null || numbers.isEmpty()) return Int.MAX_VALUE
-    if (numbers.size == 1) return  numbers[0]
-
-    var first = 0
-    var end = numbers.size-1
-    if (numbers[first] < numbers[end] ) return  numbers[first]
-    while (first <= end){
-        val mid = first+(end-first)/2
-        if (numbers[mid] > numbers[mid+1]) return numbers[mid+1]
-        if (numbers[mid-1] > numbers[mid]) return numbers[mid]
-
-        if (numbers[mid] > numbers[0]) {
-            first = mid + 1;
-        } else {
-            end = mid - 1;
+    var low = 0
+    var high = numbers.size - 1
+    while (low < high) {//*****
+        val pivot = low + ((high - low) shr 1)
+        when {
+            numbers[pivot] < numbers[high] -> high = pivot
+            numbers[pivot] > numbers[high] -> low = pivot + 1
+            else -> high -= 1
         }
     }
-    return return Int.MAX_VALUE
+    return numbers[low]
 }
