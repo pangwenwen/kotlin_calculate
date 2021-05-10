15ff4cec7bef4e33063c1dc990f5485fdf944801
panghua <panghua@le.com>
2021-05-10 10:18:26 +0800

    查找大于半数的值

---
+++ Calculate_Kotlin/src/main/java/kotlin/arryaOp/majoriEle_1710.kt
@@ -0,0 +1,25 @@
+package kotlin.arryaOp
+
+//https://leetcode-cn.com/problems/find-majority-element-lcci/
+// 摩尔投票法
+fun majorityElement(nums: IntArray): Int {
+    if(nums.isEmpty()) return -1
+    var count = 0
+    var value = -1
+    for (i in nums){
+        if (count == 0){
+            count++
+            value = i
+        }else{
+            if (i == value) count++
+            else count--
+        }
+    }
+
+    count = 0
+    for (i in nums){
+        if (i == value) count++
+    }
+    if (count > (nums.size shr 1)) return value
+    return -1
+}
