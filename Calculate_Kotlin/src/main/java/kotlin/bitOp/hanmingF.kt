7174a69bd3c4dc85d858e4a2f756320057e98bc8
panghua <panghua@le.com>
2021-06-13 09:02:38 +0800

    数组的汉明距离

---
+++ Calculate_Kotlin/src/main/java/kotlin/bitOp/hanmingF.kt
@@ -0,0 +1,15 @@
+package kotlin.bitOp
+
+fun totalHammingDistance(nums: IntArray): Int {
+    var res = 0
+    val n = nums.size
+    for(num in 0..29){
+        var c = 0
+        for (i in nums){
+            c += (i ushr num) and 1
+        }
+        res += c * (n-c)
+
+    }
+    return res
+}
