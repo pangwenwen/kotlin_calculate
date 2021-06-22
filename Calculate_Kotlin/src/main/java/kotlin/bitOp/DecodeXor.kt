7367a7d9c5b659de5d541cc196b3b675855230d1
panghua <panghua@le.com>
2021-06-22 10:07:14 +0800

    数组xor后求原数组

---
+++ Calculate_Kotlin/src/main/java/kotlin/bitOp/DecodeXor.kt
@@ -0,0 +1,11 @@
+package kotlin.bitOp
+//https://leetcode-cn.com/problems/decode-xored-array/
+fun decode(encoded: IntArray, first: Int): IntArray {
+    val size = encoded.size
+    var decode = IntArray(size+1)
+    decode[0] = first
+    for (i in 0 until size){
+        decode[i+1] = decode[i] xor encoded[i]
+    }
+    return decode
+}
