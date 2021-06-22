e6b7bd92966621114806df99e12d299cf776a7f2
panghua <panghua@le.com>
2021-06-22 09:52:21 +0800

    数组中质数个1的数子个数

--- Calculate_Kotlin/src/main/java/kotlin/bitOp/fourM.kt
+++ Calculate_Kotlin/src/main/java/kotlin/bitOp/fourM.kt
@@ -1,4 +1,4 @@
-package kotlin.bitOp
+package bitOp
 fun isPowerOfFour(n: Int): Boolean {
     return (n>0) and (n and (n-1) == 0) and (n and (0xaaaaaaaa.toInt()) == 0)
 }
@@ -38,3 +38,23 @@ fun swapNumbers2(numbers: IntArray): IntArray {
     numbers[0] = numbers[0] xor numbers[1]
     return numbers
 }
+
+// https://leetcode-cn.com/problems/prime-number-of-set-bits-in-binary-representation/
+fun countPrimeSetBits(left: Int, right: Int): Int {
+    var count = 0
+    for (i in left..right) {
+        if (isP(Integer.bitCount(i))) {
+            count++
+        }
+    }
+    return count
+}
+
+private fun isP(count: Int): Boolean {
+    return (count == 2 || count == 3 || count == 5 || count == 7 || count == 11
+            || count == 13 || count == 17 || count == 19)
+}
+
+
+
+
